9b50f6631630aed5685eb169ff18e8ee768095be
Angela Lim <angela.lim57@myhunter.cuny.edu>
2018-10-01 13:38:31 -0400

    adding the files for project 3

---
+++ Project3/PlayList.cpp
@@ -0,0 +1,16 @@
+#include "PlayList.h"
+#include "Song.h"
+#include "LinkedSet.h"
+#include <iostream>
+#include <vector>
+
+Playlist::Playlist(){
+  tail_ptr_(nullptr);
+}
+PlayList::PlayList(const Song& a_song){
+
+}
+
+PlayList::~PlayList(){
+  
+}
